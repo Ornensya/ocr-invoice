e71584e4213b844bcf6f4e73ddaeaf68b63c1be2
ornensyasembiring <ornensyasembiring@gmail.com>
2025-04-08 21:52:55 +0700

    update halaman home

--- Home.py
+++ Home.py
@@ -2,7 +2,10 @@ import streamlit as st
 
 st.set_page_config(page_title="Menu Utama", layout="wide")
 
-st.title("📊 Aplikasi Analisis Data")
+st.title("📊 Aplikasi OCR CV & INVOICE")
 
 st.markdown("____")
-st.markdown("Penjelasan aplikasi")
+container = st.container(border=True)
+container.write('''Selamat datang di aplikasi OCR CV & Invoice Analyzer – solusi cerdas untuk mengubah dokumen cetak menjadi data digital yang mudah diproses! 
+                Aplikasi ini dirancang untuk membantu perusahaan, tim HR, dan bagian keuangan dalam memproses dokumen seperti Curriculum Vitae (CV) dan Invoice (faktur) 
+                secara otomatis menggunakan teknologi Optical Character Recognition (OCR).''')
